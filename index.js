0e3ad3b3924e899c00b2a4e71b93876251e2e3be
Abed Nganga <ngangaabed@gmail.com>
2023-06-08 13:47:48 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solutions in this file
+/*
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+
+  }
+
+  return gifts;
+}
+
+wrapGifts(gifts);
+*/
+
+const cards = []; //array to hold the card messages
+function writeCards(name) {
+        for (let i = 0; i < name.length; i++) {
+          const message = `Thank you, ${name[i]}, for the wonderful surprise gift!`;
+          cards.push(message); //append message at the end
+        }
+        return cards;
+      }
+      
+function countDown (name) {
+    for (let i=10; i>=0; i--) {
+        console.log(i);
+    }
+}
+      
+      
+      
+      
+      
